7cb3fa3f677df23b82cefd34a1f66f6ec0c0e7f5
Bjoern <23384292+bc-bjoern@users.noreply.github.com>
2026-03-20 18:50:28 +0100

    version handling

--- src/todoctl/__init__.py
+++ src/todoctl/__init__.py
@@ -5,5 +5,7 @@ This module exposes the public package version and defines the
 minimal public interface that should be available when importing
 the package root.
 """
-__all__ = ["__version__"]
-__version__ = "1.0.0"
+# src/todoctl/__init__.py
+from importlib.metadata import version
+
+__version__ = version("todoctl")
